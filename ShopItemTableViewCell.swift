e74ae3a6952836c01ce6c743b844435d619ecf88
曽和寛貴 <hiro.0723boss.macpro@gmail.com>
2017-09-09 14:57:00 +0900

    [add] TableViewの設定

---
+++ ShopItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShopItemTableViewCell.swift
+//  GourmetSearch3
+//
+//  Created by 曽和寛貴 on 2017/09/09.
+//  Copyright © 2017年 曽和寛貴. All rights reserved.
+//
+
+import UIKit
+
+class ShopItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
