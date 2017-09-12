d3b75981f66959e69a24143e8a1116904a158ff0
曽和寛貴 <hiro.0723boss.macpro@gmail.com>
2017-09-12 11:07:07 +0900

    [add] ShpItemTableViewCellへの値の設定

--- ShopItemTableViewCell.swift
+++ ShopItemTableViewCell.swift
@@ -22,6 +22,47 @@ class ShopItemTableViewCell: UITableViewCell {
     @IBOutlet weak var stationX: NSLayoutConstraint!
     
     
+    var shop: Shop = Shop() {
+        
+        didSet {
+            
+            name.text = shop.name
+            var x: CGFloat = 0
+            let margin: CGFloat = 10
+            if shop.hasCoupon {
+                coupon.isHidden = false
+                x += coupon.frame.size.width + margin
+                
+                coupon.layer.cornerRadius = 4
+                coupon.clipsToBounds = true
+            } else {
+                coupon.isHidden = true
+            }
+            
+            if shop.station != nil {
+                station.isHidden = false
+                station.text = shop.station
+                stationX.constant = x
+                
+                let size = station.sizeThatFits(CGSize(
+                    width: CGFloat.greatestFiniteMagnitude,
+                    height: CGFloat.greatestFiniteMagnitude))
+                if x + size.width + margin > iconContainer.frame.width {
+                    stationWidth.constant = iconContainer.frame.width - x
+                } else {
+                    stationWidth.constant = size.width + margin
+                }
+                
+                station.clipsToBounds = true
+                station.layer.cornerRadius = 4
+            } else {
+                station.isHidden = true
+            }
+        }
+    
+    }
+    
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -32,6 +73,19 @@ class ShopItemTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let maxFrame = CGRect (
+            x: 0,
+            y: 0,
+            width: name.frame.size.width,
+            height: CGFloat.greatestFiniteMagnitude)
+        let actualFrame = name.textRect(forBounds: maxFrame, limitedToNumberOfLines: 2)
+        
+        nameHeight.constant = actualFrame.size.height
     }
 
 }
