df039f6b23e338a6b91224d812df050ddbd8271b
曽和寛貴 <hiro.0723boss.macpro@gmail.com>
2017-09-12 16:59:17 +0900

    [add] 画面遷移の実装

---
+++ FreewordTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  FreewordTableViewCell.swift
+//  GourmetSearch3
+//
+//  Created by 曽和寛貴 on 2017/09/12.
+//  Copyright © 2017年 曽和寛貴. All rights reserved.
+//
+
+import UIKit
+
+class FreewordTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var freeword: UITextField!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
