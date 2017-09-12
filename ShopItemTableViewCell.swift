3b09471ac83459e85919a6b2feb616595bfefd78
曽和寛貴 <hiro.0723boss.macpro@gmail.com>
2017-09-12 13:31:04 +0900

    [add] 画像の読み込み

--- ShopItemTableViewCell.swift
+++ ShopItemTableViewCell.swift
@@ -25,7 +25,16 @@ class ShopItemTableViewCell: UITableViewCell {
     var shop: Shop = Shop() {
         
         didSet {
+//            URLがあれば画像を表示する
+            if let url = shop.photoUrl{
+                photo.sd_cancelCurrentAnimationImagesLoad()
+                photo.sd_setImage(
+                    with: URL(string: url),
+                    placeholderImage: UIImage(named: "loading"),
+                        options: .retryFailed)
+            }
             
+//            店舗名をラベルに設定
             name.text = shop.name
             var x: CGFloat = 0
             let margin: CGFloat = 10
