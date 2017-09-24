3e643bf83b8b276175e3f2afa044e70a76f1dd02
曽和寛貴 <hiro.0723boss.macpro@gmail.com>
2017-09-24 13:21:37 +0900

    [add] コメントアウトの追加

--- ShopMapDetailViewController.swift
+++ ShopMapDetailViewController.swift
@@ -55,74 +55,79 @@ class ShopMapDetailViewController: UIViewController {
     
     
     override func viewWillAppear(_ animated: Bool) {
-        
+//        位置情報の取得を禁止している場合
         observers.append(
             nc.addObserver(forName: .authDenied, object: nil, queue: nil, using: {
                 notification in
                 
-                
+//                位置情報がonになっていないダイアログ表示
                 self.present(self.ls.locationServiceDisabledAlert,
                              animated: true,
                              completion: nil)
-                
+//                現在地を表示を非アクティブにする
                 self.showHereButton.isEnabled = false
             })
         )
         
-        
+//        位置情報取得を制限している場合
         observers.append(
             nc.addObserver(forName: .authRestricted, object: nil, queue: nil, using: {
                 notification in
                 
-                
+//                位置情報が制限されているダアイログ表示
                 self.present(self.ls.locationServiceRestrictedAlert,
                              animated: true,
                              completion: nil)
                 
+//                現在地を表示ボタンを非アクティブにする
                 self.showHereButton.isEnabled = false
             })
         )
 
         
-        
+//        位置情報取得に失敗した場合
         observers.append(
             nc.addObserver(forName: .didFailLocation, object: nil, queue: nil, using: {
                 notification in
                 
-                
+//                位置情報取得に失敗したダイアログ
                 self.present(self.ls.locationServiceDidFailAlert,
                              animated: true,
                              completion: nil)
-                
+//                現在地を表示を非アクティブ
                 self.showHereButton.isEnabled = false
             })
         )
         
-        
+//        位置情報を取得したい場合
         observers.append(
             nc.addObserver(forName: .didUpdateLocation, object: nil, queue: nil, using: {
                 notification in
                 
+//                [現在地を表示]ボタンをアクティブ
                 self.showHereButton.isEnabled = false
                 
-                
+//                位置情報が渡されていなければ早期離脱
                 guard let userInfo = notification.userInfo as? [String: CLLocation] else {
                     return
                 }
                 
+//                userInfoがキーlocationを持っていなければ早期離脱
                 guard let clloc = userInfo["location"] else {
                     return
                 }
                 
+//                店舗が位置情報を持っていなければ早期離脱
                 guard let lat = self.shop.lon else {
                     return
                 }
-                
+            
                 guard let lon  = self.shop.lon else {
                     return
                 }
                 
                 
+//                地図の表示範囲を設定する
                 let center = CLLocationCoordinate2D(
                     latitude: (lat + clloc.coordinate.latitude) / 2,
                     longitude: (lon + clloc.coordinate.longitude) / 2)
@@ -131,22 +136,28 @@ class ShopMapDetailViewController: UIViewController {
                     lat: abs(clloc.coordinate.latitude - lat),
                     lon: abs(clloc.coordinate.longitude - lon))
                 
+                
+//                表示範囲を設定する
                 let mkcs = MKCoordinateSpanMake(diff.lat * 1.4, diff.lon * 1.4)
                 let mkcr = MKCoordinateRegionMake(center, mkcs)
                 self.map.setRegion(mkcr, animated: true)
             
+//                現在地を表示する
+                self.map.showsUserLocation = true
             })
         )
         
+//        位置情報が利用可能になった場合
         observers.append(
             nc.addObserver(forName: .authorized, object: nil, queue: nil, using: {
                 notification in
                 
+//                [現在地の表示]ボタンをアクティブにする
                 self.showHereButton.isEnabled = true
             })
         )
     }
-    
+//    Notificationの待受けを解除する
     override func viewWillDisappear(_ animated: Bool) {
         
         for observer in observers {
