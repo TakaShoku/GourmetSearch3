7e664486f0e6b9bfe265809d9f386330f4dc4198
曽和寛貴 <hiro.0723boss.macpro@gmail.com>
2017-09-29 17:36:24 +0900

    [mod] アプリについての画面

---
+++ AboutViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AboutViewController.swift
+//  GourmetSearch3
+//
+//  Created by 曽和寛貴 on 2017/09/29.
+//  Copyright © 2017年 曽和寛貴. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 110
+        }
+        return 44
+    }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AboutApp", for: indexPath)
+            return cell
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "License", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+}
